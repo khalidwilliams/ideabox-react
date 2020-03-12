fffb8d71fa7284e380ee56f3d57e041bceec44b6
Khalid Williams <khalidwill27@gmail.com>
2020-03-11 21:44:46 -0600

    Test and implement From component

--- src/Form.js
+++ src/Form.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const Form = ({addCard}) => {
   return (
-    <form></form>
+    <form>
+      <div className="label-group">
+        <label for="title-input">Idea Title</label>
+        <input id="title-input" type="text" />
+      </div>
+      <div className="label-group">
+        <label for="description-input">Idea Title</label>
+        <input id="description-input" type="text"/>
+      </div>
+      <button className="add-card-btn" onClick={() => addCard()}>Add Idea</button>
+    </form>
   );
 };
 
--- src/Form.test.js
+++ src/Form.test.js
@@ -7,12 +7,22 @@ describe('Form Component', () => {
   beforeEach(() => {
     addCardMock = jest.fn();
   });
+
   it('should render a form', () => {
     const shallowForm = shallow(<Form 
         addCard={addCardMock}
       />);
-    const formElement = <form></form>;
 
-    expect(shallowForm.contains(formElement)).toEqual(true);
+    expect(shallowForm).toMatchSnapshot();
   });
+
+  it('should add a card', () => {
+    const shallowForm = shallow(<Form 
+        addCard={addCardMock}
+      />);
+    shallowForm.find('.add-card-btn').simulate('click');
+    expect(addCardMock).toHaveBeenCalledTimes(1);
+  });
+  
+  
 });
